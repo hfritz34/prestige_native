2c98bc7bfd2cebfa0aa17b879282536d580a4d2d
hfritz34 <henryfritz00@gmail.com>
2025-04-07 09:17:04 -0400

    Initial Xcode project setup

---
+++ PrestigeNative/PrestigeNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrestigeNativeApp.swift
+//  PrestigeNative
+//
+//  Created by Henry Fritz on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PrestigeNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrestigeNativeTests/PrestigeNativeTests.swift
@@ -0,0 +1,16 @@
+//
+//  PrestigeNativeTests.swift
+//  PrestigeNativeTests
+//
+//  Created by Henry Fritz on 4/7/25.
+//
+
+import Testing
+
+struct PrestigeNativeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
